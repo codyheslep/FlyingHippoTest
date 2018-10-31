4c40b29f6e13f4b4b74295aaa63464ad98c90943
Cody Heslep <codyheslep@gmail.com>
2018-10-31 16:18:46 -0500

    layout of sidebar

--- client/components/sidebar.js
+++ client/components/sidebar.js
@@ -7,15 +7,44 @@ const Sidebar = () => {
     <div className="sidebarContainer">
       <div className="latestPosts">
         <div className="sidebarHeader">
+        <text>LATEST POSTS FROM</text>
+        <text>MELLANIE</text>
         </div>
         <div className="latestPostsContent">
-        <img src="../images/yellow_texture.png" className="latestPostsContentBackground" />
+        <div className="posts">
+          <text>sample post</text>
+          <br/>
+          <text>date posted</text>
+          <hr/>
+          <text>sample post</text>
+          <br/>
+          <text>date posted</text>
+          <hr/>
+          <text>sample post</text>
+          <br/>
+          <text>date posted</text>
+          <hr/>
+          <text>sample post</text>
+          <br/>
+          <text>date posted</text>
+          <hr/>
+          <text>sample post</text>
+          <br/>
+          <text>date posted</text>
+          <hr/>
+          <button/>
+          </div>
+          <img src="../images/yellow_texture.png" className="latestPostsContentBackground" />
         </div>
       </div>
       <div className="latestVideos">
-        <div className="sidebarHeader">
+        <div className="sidebarHeader" >
+        <text>Latest Videos by</text>
+        <text>Mellanie</text>
         </div>
-        <div className="latestVideosContent">
+        <div>
+        <div className="latestVideosContentBackground"/>
+          <img src="../images/tv_sketch.png" className="latestVideosContent" />
         </div>
       </div>
     </div>
